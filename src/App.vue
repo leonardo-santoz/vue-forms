1e4f3bc7b321b161346523eaefd8eb9706583c93
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-10-21 09:30:06 -0300

    armazenando valores de checkboxes em arrays

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
 <div id="app">
-
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
             <h1 class="display-4">Formulários no Vue</h1>
@@ -9,45 +8,39 @@
     </div>
 
     <div class="container">
-
         <div class="row">
-
             <!-- formulario -->
             <div class="col-sm-6">
-
                 <h3>Preencha abaixo</h3>
 
                 <form>
-
                     <div class="form-group">
                         <label>Nome:</label>
-                        <input type="text" class="form-control" placeholder="Seu nome" v-model.lazy.trim="desenvolvedor.nome">
+                        <input type="text" class="form-control" placeholder="Seu nome" v-model.lazy.trim="desenvolvedor.nome" />
                     </div>
 
                     <div class="form-group">
                         <label>Endereço de email:</label>
-                        <input type="email" class="form-control" placeholder="Seu email" v-model.lazy="desenvolvedor.email">
+                        <input type="email" class="form-control" placeholder="Seu email" v-model.lazy="desenvolvedor.email" />
                     </div>
 
                     <div class="form-group">
                         <label>Idade:</label>
-                        <input type="number" class="form-control" placeholder="Sua idade" v-model.number="desenvolvedor.idade">
+                        <input type="number" class="form-control" placeholder="Sua idade" v-model.number="desenvolvedor.idade" />
                     </div>
 
                     <div class="form-group">
-
                         <p>Gênero:</p>
 
                         <div class="form-check form-check-inline">
-                            <input type="radio" class="form-check-input" value="Masculino" v-model="desenvolvedor.genero">
+                            <input type="radio" class="form-check-input" value="Masculino" v-model="desenvolvedor.genero" />
                             <label class="form-check-label">Masculino</label>
                         </div>
 
                         <div class="form-check form-check-inline">
-                            <input type="radio" class="form-check-input" value="Feminino" v-model="desenvolvedor.genero">
+                            <input type="radio" class="form-check-input" value="Feminino" v-model="desenvolvedor.genero" />
                             <label class="form-check-label">Feminino</label>
                         </div>
-
                     </div>
 
                     <div class="form-group">
@@ -58,75 +51,85 @@
                     </div>
 
                     <div class="form-group">
-
                         <p>Tecnologias:</p>
 
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" value="JavaScript">
+                            <input type="checkbox" class="form-check-input" value="JavaScript" v-model="desenvolvedor.tecnologias" />
                             <label class="form-check-label">JavaScript</label>
                         </div>
 
-                        <div class="form-check form-check-inline" value="Vue JS">
-                            <input type="checkbox" class="form-check-input">
+                        <div class="form-check form-check-inline">
+                            <input type="checkbox" class="form-check-input" value="Vue JS" v-model="desenvolvedor.tecnologias" />
                             <label class="form-check-label">Vue JS</label>
                         </div>
 
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" value="Vuex">
+                            <input type="checkbox" class="form-check-input" value="Vuex" v-model="desenvolvedor.tecnologias" />
                             <label class="form-check-label">Vuex</label>
                         </div>
 
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" value="Vue Router">
+                            <input type="checkbox" class="form-check-input" value="Vue Router" v-model="desenvolvedor.tecnologias" />
                             <label class="form-check-label">Vue Router</label>
                         </div>
-
                     </div>
 
                     <div class="form-group">
                         <label>Resumo de perfil:</label>
                         <textarea class="form-control" placeholder="Conte-nos um pouco sobre você..." v-model="desenvolvedor.bio">
-                        </textarea>
+              </textarea>
                     </div>
 
                     <div class="form-group">
-
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" v-model="desenvolvedor.notificacoes" true-value="Sim" false-value="Não">
+                            <input type="checkbox" class="form-check-input" v-model="desenvolvedor.notificacoes" true-value="Sim" false-value="Não" />
                             <label class="form-check-label">Receber notificações por email</label>
                         </div>
-
                     </div>
 
                     <button class="btn btn-secondary">Resetar</button>
                     <button class="btn btn-success">Enviar</button>
-
                 </form>
-
             </div>
 
             <!-- saida -->
             <div class="col-sm-6">
-
                 <h3>Saída</h3>
 
                 <div class="card">
-
                     <div class="card-header">Dados</div>
 
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><strong>Nome:</strong> {{ desenvolvedor.nome }}</li>
-                        <li class="list-group-item"><strong>Email:</strong> {{ desenvolvedor.email }} </li>
-                        <li class="list-group-item"><strong>Idade:</strong> {{ desenvolvedor.idade }} </li>
-                        <li class="list-group-item"><strong>Gênero:</strong> {{ desenvolvedor.genero }}</li>
-                        <li class="list-group-item"><strong>Ocupação:</strong> </li>
-                        <li class="list-group-item"><strong>Tecnologias:</strong> </li>
+                        <li class="list-group-item">
+                            <strong>Nome:</strong> {{ desenvolvedor.nome }}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Email:</strong> {{ desenvolvedor.email }}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Idade:</strong> {{ desenvolvedor.idade }}
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Gênero:</strong> {{ desenvolvedor.genero }}
+                        </li>
+                        <li class="list-group-item"><strong>Ocupação:</strong></li>
+                        <li class="list-group-item">
+                            <strong>Tecnologias:</strong>
+                            <ul>
+                                <li v-for="(tecnologia, indice) in desenvolvedor.tecnologias" :key="indice">
+                                    {{ tecnologia }}
+                                </li>
+                            </ul>
+                        </li>
                         <li class="list-group-item">
                             <strong>Biografia:</strong>
                             <!-- <pre>{{ desenvolvedor.bio }} </pre> -->
-                            <div style="white-space: pre"> {{ desenvolvedor.bio }} </div>
+                            <div style="white-space: pre">{{ desenvolvedor.bio }}</div>
                         </li>
-                        <li class="list-group-item"><strong>Receber notificações?</strong> {{ desenvolvedor.notificacoes }} </li>
+                        <li class="list-group-item">
+                            <strong>Receber notificações?</strong>
+                            {{ desenvolvedor.notificacoes }}
+                        </li>
                     </ul>
 
                     <div class="card-header">Model</div>
@@ -134,15 +137,10 @@
                     <div class="card-body">
                         <pre><code>{{ { desenvolvedor } }}</code></pre>
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     </div>
-
 </div>
 </template>
 
@@ -151,16 +149,17 @@ export default {
     data() {
         return {
             desenvolvedor: {
-                nome: '',
-                email: '',
+                nome: "",
+                email: "",
                 idade: 28,
-                bio: 'Sou desenvolvedor desde 2010...',
-                genero: 'Feminino',
-                notificacoes: 'Não'
-            }
-        }
-    }
-}
+                bio: "Sou desenvolvedor desde 2010...",
+                genero: "Feminino",
+                tecnologias: [],
+                notificacoes: "Não",
+            },
+        };
+    },
+};
 </script>
 
 <style scoped>
